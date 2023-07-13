44e85b9ad6af83ec2e5a977ac68a9a29d751507f
Aryan <aryanm468@gmail.com>
2023-07-13 13:21:04 +0530

    Re: Implementation of toggle switch button for camera permissions

--- app/src/main/java/com/example/task2/MainActivity.kt
+++ app/src/main/java/com/example/task2/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.task2
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,18 +15,21 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.app.ActivityCompat.shouldShowRequestPermissionRationale
+import androidx.core.content.ContextCompat.startActivity
 import com.example.task2.ui.theme.Task2Theme
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberPermissionState
-import com.google.accompanist.permissions.shouldShowRationale
 
 
 class MainActivity : ComponentActivity() {
@@ -37,29 +45,11 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
-fun LoadPerms(modifier: Modifier = Modifier) {
+fun LoadPerms() {
 
     val cameraPermissionState = rememberPermissionState(
         android.Manifest.permission.CAMERA
     )
-
-    DrawUI(
-        modifier,
-        requestPermissions = {
-            cameraPermissionState.launchPermissionRequest()
-        },
-        cameraPermissionState.status.isGranted,
-        cameraPermissionState.status.shouldShowRationale
-    )
-}
-
-@Composable
-fun DrawUI(
-    modifier: Modifier,
-    requestPermissions: () -> Unit,
-    isCameraPermissionGranted: Boolean,
-    shouldShowRationale: Boolean
-) {
 
     val checkedState = rememberSaveable {
         mutableStateOf(false)
@@ -69,15 +59,38 @@ fun DrawUI(
         mutableStateOf("")
     }
 
+    val context = LocalContext.current as Activity
+    val permsLauncher =
+        rememberLauncherForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                checkedState.value = true
+                permissionStatusTextState.value = "Camera permission Granted"
+                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivity(context, intent, null)
+            } else {
+                checkedState.value = false
+                if (shouldShowRequestPermissionRationale(
+                        context,
+                        android.Manifest.permission.CAMERA
+                    )
+                ) {
+                    permissionStatusTextState.value =
+                        "The camera is important for this app.\nPlease grant the permission."
+                } else {
+                    permissionStatusTextState.value = "Camera permission Denied"
+                }
+            }
+        }
+
     Column(
-        modifier.fillMaxSize(),
+        Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     )
     {
         LoadSwitchStateless(checkedState.value, onCheckedChange = { value ->
             if (value) {
-                requestPermissions()
+                permsLauncher.launch(android.Manifest.permission.CAMERA)
             } else {
                 checkedState.value = false
             }
@@ -85,16 +98,15 @@ fun DrawUI(
         LoadTextStateless(permissionStatusTextState.value)
     }
 
-    if (isCameraPermissionGranted) {
-        checkedState.value = true
-        permissionStatusTextState.value = "Camera permission Granted"
-    } else {
-        checkedState.value = false
-        if (shouldShowRationale) {
-            permissionStatusTextState.value =
-                "The camera is important for this app.\nPlease grant the permission."
-        } else {
-            permissionStatusTextState.value = "Camera permission Denied"
+    LaunchedEffect(Unit) {
+        when {
+            cameraPermissionState.status.isGranted -> {
+                // Camera permission already granted
+            }
+
+            else -> {
+                // Request camera permission
+            }
         }
     }
 }
